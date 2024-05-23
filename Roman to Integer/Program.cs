4b38467e445656bd7a7bfad2851279a5ccd16100
Amir-Jalali <923.infinity.923@gmail.com>
2024-05-23 19:16:28 +0330

    solved Roman to Integer

---
+++ Roman to Integer/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine();
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> table = new Dictionary<char, int>
+            {
+                {'I', 1 },
+                {'V', 5},
+                {'X', 10},
+                {'L', 50 },
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+        int sum = 0;
+        int last = table[s[0]];
+        foreach (char c in s)
+        {
+            if (table[c] > last)
+                sum = sum - last + table[c] - last;
+            else
+                sum += table[c];
+            last = table[c];
+        }
+        return sum;
+    }
+}
