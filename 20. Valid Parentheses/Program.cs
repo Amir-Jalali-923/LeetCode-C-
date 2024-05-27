37a8b1191a99cb182d59e0e39e6b7056122d5e9e
Amir-Jalali <923.infinity.923@gmail.com>
2024-05-27 20:47:21 +0330

    Solved "20. Valid Parentheses"

---
+++ 20. Valid Parentheses/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Solution.IsValid("(){}[]"));
+
+public class Solution
+{
+    public static bool IsValid(string str)
+    {
+        Dictionary<char, char> matches = new Dictionary<char, char>
+            {
+                {'(', ')'},
+                {'[', ']'},
+                {'{', '}'}
+            };
+        Stack<char> stk = new Stack<char>();
+        char[] opens = new char[] { '(', '{', '[' };
+        foreach (char c in str)
+        {
+            if (opens.Contains(c))
+                stk.Push(c);
+            else
+            {
+                if (stk.Count == 0 || c != matches[stk.Pop()])
+                    return false;
+            }
+        }
+        return stk.Count == 0 ? true : false;
+    }
+}
