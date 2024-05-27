3c59e0525e880fe015588de99efa3a3ebbcae90c
Amir-Jalali <923.infinity.923@gmail.com>
2024-05-27 20:24:30 +0330

    Solved "Longest Common Prefix"

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Solution.LongestCommonPrefix(new string[]{"flower", "flow", "flight"}));
+public class Solution
+{
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 1)
+            return strs[0];
+
+        int index = 0;
+        string CP = "";
+        while (index < strs[0].Length)
+        {
+            foreach (string str in strs)
+                if (str.Length == index || str[index] != strs[0][index])
+                    return CP;
+            CP += strs[0][index];
+            index++;
+        }
+        return CP;
+    }
+}
