113b2fdbcafe3057167a8dc01faec83e2e74cf5e
Amir-Jalali <923.infinity.923@gmail.com>
2024-05-28 14:06:46 +0330

    Solved "26. Remove Duplicates from Sorted Array"

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int max = nums.Max();
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i - 1] >= nums[i])
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] > nums[i - 1])
+                    {
+                        nums[i] = nums[j];
+                        break;
+                    }
+                }
+            }
+            if (nums[i] == max)
+            {
+                return nums[i - 1] == nums[i] ? i : i + 1;
+            }
+        }
+        return 1;
+    }
+}
