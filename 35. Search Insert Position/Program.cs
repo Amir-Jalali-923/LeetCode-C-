0efae26209d9c7fcc476337b86915b56daf800bc
Amir-Jalali <923.infinity.923@gmail.com>
2024-05-30 16:19:17 +0330

    Solved "35. Search Insert Position"

---
+++ 35. Search Insert Position/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine(Solution.SearchInsert(new int[] { 1, 3, 5, 6 }, 7));
+public class Solution
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        int L = 0; int R = nums.Length; int mid;
+        while (L < R)
+        {
+            mid = (R + L) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                R = mid - 1;
+            else
+                L = mid + 1;
+        }
+        return L < nums.Length && nums[L] < target ? L + 1 : L;
+    }
+}
