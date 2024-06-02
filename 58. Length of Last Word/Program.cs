4622ea16775ee060787f1a38f1a4a66d623393f8
Amir-Jalali <923.infinity.923@gmail.com>
2024-06-02 14:09:27 +0330

    Solved "58. Length of Last Word"

---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(Solution.LengthOfLastWord("Hello world"));
+
+public class Solution
+{
+    public static int LengthOfLastWord(string s)
+    {
+        bool foundLetter = false;
+        int wordEnd = -1;
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ' && foundLetter)
+                return wordEnd - i;
+            else if (s[i] != ' ' && !foundLetter)
+            {
+                foundLetter = true;
+                wordEnd = i;
+            }
+        }
+        return wordEnd == -1 ? 0 : wordEnd + 1;
+    }
+}
