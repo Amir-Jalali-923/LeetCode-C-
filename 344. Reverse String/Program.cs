afd6efdaab4091ee74750370ac8eb917ab313533
Amir-Jalali <923.infinity.923@gmail.com>
2024-06-02 16:22:43 +0330

    Solved "344. Reverse String"

---
+++ 344. Reverse String/Program.cs
@@ -0,0 +1,16 @@
+﻿Solution.ReverseString("Hello World".ToCharArray());
+public class Solution
+{
+    public static void ReverseString(char[] s)
+    {
+        int L = 0;
+        int R = s.Length - 1;
+        char tmp;
+        while (L < R)
+        {
+            tmp = s[L];
+            s[L++] = s[R];
+            s[R--] = tmp;
+        }
+    }
+}
