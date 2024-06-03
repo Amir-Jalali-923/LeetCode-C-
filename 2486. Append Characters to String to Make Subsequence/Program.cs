db124855cd11070282a0be39b3a06a9d657b4336
Amir-Jalali <923.infinity.923@gmail.com>
2024-06-03 13:03:21 +0330

    Solved "2486. Append Characters to String to Make Subsequence"

---
+++ 2486. Append Characters to String to Make Subsequence/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine(Solution.AppendCharacters("abcd","a"));
+
+public class Solution
+{
+    public static int AppendCharacters(string s, string t)
+    {
+        int j = 0;
+        for (int i = 0; i < s.Length && j < t.Length; i++)
+        {
+            if (s[i] == t[j])
+                j++;
+        }
+        return t.Length - j;
+    }
+}
